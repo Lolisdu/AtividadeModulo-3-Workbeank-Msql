58ae70d28c0843c68612b88f0af1972428f5320d
Lolisdu <lolidu@yahoo.com.br>
2022-10-11 12:28:44 -0300

    Atualização 11/10
    Atualização de teste e efetivação do Banco de Dados

--- MyBD_viagem.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE Usuario (
-    id_permissao INTEGER PRIMARY KEY auto_increment,
-    nome_cliente VARCHAR (30),
-    cpf_cliente int,
-    email_cliente VARCHAR (30)
-);
---
+++ atividade_viagem_recode.sql
@@ -0,0 +1,9 @@
+
+
+insert into compra (id, origem, destino, qtdade_pessoa, valor)
+values (3, "São Paulo", "Londres", "2", "350.00" );
+
+select *from compra;
+
+
+
